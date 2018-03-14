c443f5961e1efc79af477d952b1b9f2b72370382
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2018-03-14 15:39:44 +0900

    123

--- ButtonLR/ViewController.swift
+++ ButtonLR/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
-
+var a = 1
+var b = 1
+var c = 1
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RLlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btLeft(_ sender: UIButton) {
+        if a == 1 {
+            RLlabel.text = "Left Button Pressed"
+            a = 1
+        }
+    }
+    
+    @IBAction func btRight(_ sender: UIButton) {
+        if b == 1 {
+            RLlabel.text = "Right Button Pressed"
+            b = 1
+        }
+    }
+    
+    @IBAction func btReset(_ sender: UIButton) {
+        if c == 1 {
+            RLlabel.text = "Select Button"
+            c = 1
+        }
+    }
 }
 
